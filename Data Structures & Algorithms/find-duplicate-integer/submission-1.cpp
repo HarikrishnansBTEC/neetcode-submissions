58b7cd730879b66b8bd970c70a9fabef9c25660a
HarikrishnansBTEC <harikrishnans095@gmail.com>
2026-05-10 18:21:05 +0530

    Add: find-duplicate-integer - submission-1

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-1.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        
+        unordered_map<int,bool> hash;
+        int result = -1;
+        for(int i : nums)
+        {
+            if(hash[i])
+            {
+                result =  i;
+                break;
+            }
+            hash[i] = true;
+        }
+        return result;
+    }
+};
